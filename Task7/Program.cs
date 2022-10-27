49e91cdf9fc4f32a91cbe5ede61b0f735854b9d7
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 22:53:21 +0300

    task7 upd

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,10 +1,7 @@
 ﻿// принимает на вход трёхзначное число
 //и на выходе показывает последнюю цифру этого числа
-Console.WriteLine("Enter number: ");
+Console.WriteLine("Enter three-digit number: ");
 int num;
 num = Convert.ToInt32(Console.ReadLine());
-
-if(num == 1)
-{
-    Console.WriteLine($"Monday");
-}
+int lastNum = num % 10;
+Console.WriteLine("The last digit of the number: " + lastNum);
