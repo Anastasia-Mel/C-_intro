85b32cd0fe4fb56b996eb0e20ff38041db7d603d
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 22:36:51 +0300

    Task 5 upd

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿// показывает все целые числа в промежутке от -N до N
+Console.WriteLine("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = -num;
+
+while(num2 <= num)
+{
+    Console.Write($"{num2}, ");
+    num2 += 1;
+}
