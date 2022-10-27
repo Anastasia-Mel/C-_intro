47052c5ea71bacb05ff86f77cdb01d182cf04b46
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 22:11:16 +0300

    tasks from lesson

--- c#_1/Program.cs
+++ c#_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter number one: ");
+int num1;
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number two: ");
+int num2;
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine($"Yes!");
+}
+else
+{
+    Console.WriteLine($"No!");
+}
