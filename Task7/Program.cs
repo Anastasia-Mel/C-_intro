2a5f736e5a309d6cd73b5c1ca5814bdb9a2ce1c7
Anastasia_Mel <melnik2775@gmail.com>
2022-10-27 22:37:09 +0300

    task7 start

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход трёхзначное число
+//и на выходе показывает последнюю цифру этого числа
+Console.WriteLine("Enter number: ");
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.WriteLine($"Monday");
+}
